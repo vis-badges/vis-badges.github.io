55dce3fe3e0817f9803b6aa6197f88d803ec3363
Valentin Edelsbrunner <v.edelsbrunner@protonmail.com>
2025-03-28 10:42:29 +0100

    Import fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
 import App from './App';
 import Compact from "./pages/Compact";
 import {createHashRouter, RouterProvider} from "react-router-dom";
