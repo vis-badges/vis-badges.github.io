4409d6f990cc375e1fcebb72ae6aa2034e4e6ed9
Valentin Edelsbrunner <v.edelsbrunner@protonmail.com>
2025-10-13 16:27:38 +0200

    Moves npm package into separate repo

--- packages/vis-badges-react/src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as BinaryBadge } from './components/BinaryBadge';
-export { default as CategoricalBadge } from './components/CategoricalBadge';
-export { default as OrdinalBadge } from './components/OrdinalBadge';
-export { default as QuantitativeBadge } from './components/QuantitativeBadge';
-
-
